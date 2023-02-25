e93af4bcb1309c919d5d21d0a25ee916348e64cf
Eugene Vorontsov <eugene.vorontsov@gmail.com>
2023-02-25 16:58:32 -0500

    Add transaction type and a way to store them.

--- database.py
+++ database.py
@@ -1,5 +1,13 @@
 from pathlib import Path
 import sqlite3
+from typing import List, NamedTuple, Optional
+
+
+class Transaction(NamedTuple):
+    date: str
+    name: str
+    amount: float
+    category: Optional[str] = None
 
 
 class database:
@@ -8,15 +16,15 @@ class database:
         self.database_file_path.parent.mkdir(parents=True, exist_ok=True)    
         self.connection = sqlite3.connect(database_file_path)
         self.cursor = self.connection.cursor()
-        self.db_name = 'bank_records'
-        self.columns = ['category', 'name', 'date', 'credit', 'debit']
+        self.table_name = 'bank_records'
+        self.columns = Transaction._fields
 
     def exists(self):
         check = self.cursor.execute(
             "SELECT name "
             "FROM sqlite_master "
             "WHERE type='table' "
-            f"AND name='{self.db_name}'"
+            f"AND name='{self.table_name}'"
         )
         db_exists = len(check.fetchall())
         assert db_exists in [0, 1]
@@ -28,5 +36,15 @@ class database:
                 'Database already exists. Will not create a new one.'
             )
         self.cursor.execute(
-            f"CREATE TABLE {self.db_name}({','.join(self.columns)})"
+            f"CREATE TABLE {self.table_name}({','.join(self.columns)})"
         )
+
+    def add_transactions(self, transaction_list: List[Transaction]):
+        for tx in transaction_list:
+            assert isinstance(tx, Transaction)
+        self.cursor.executemany(
+            f"INSERT INTO {self.table_name} VALUES (?, ?, ?, ?)",
+            transaction_list
+        )
+        self.connection.commit()
+    
