ed4f98828006fa528a52b54541540a38332f4878
Eugene Vorontsov <eugene.vorontsov@gmail.com>
2023-03-05 15:18:42 -0500

    Update database and pie chart on csv import.

--- run.py
+++ run.py
@@ -17,43 +17,109 @@ from parsing import parse_csv
 from ui.state import PieChart
 
 
+# Database path is hardcoded.
 DB_PATH = "/home/eugene/.local/bank_records/db.sql"
 
+# State is kept here.
+pie_chart = PieChart(DB_PATH)
 
+# The app accesses and updates the state.
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-
-@app.callback(Output("csv_output", "children"), Input("upload_csv", "contents"))
-def update_csv(contents_list):
-    if contents_list is not None:
-        df_list = [pd.DataFrame(columns=Transaction._fields)]
-        for contents in contents_list:
-            content_type, content_string = contents.split(",")
-            if content_type != "data:text/csv;base64":
-                continue
-            try:
-                decoded_string = io.StringIO(
-                    base64.b64decode(content_string).decode("utf-8")
-                )
-                transaction_list = parse_csv(decoded_string)
-            except Exception as e:
-                print(e)
-                pass
-            else:
-                df = pd.DataFrame.from_records(
-                    transaction_list, columns=Transaction._fields
-                )
-                df_list.append(df)
-        df = pd.concat(df_list)
-        children = html.Div(
+app.layout = html.Div(
+    children=[
+        html.Div(
             [
-                dash_table.DataTable(
-                    df.to_dict("records"),
-                    [{"name": i, "id": i} for i in df.columns],
+                dcc.Upload(
+                    id="upload_csv",
+                    children=html.Div(
+                        ["Import CSV files (click or drag and drop)"]
+                    ),
+                    style={
+                        "width": "47%",
+                        "height": "60px",
+                        "lineHeight": "60px",
+                        "borderWidth": "1px",
+                        "borderStyle": "dashed",
+                        "borderRadius": "5px",
+                        "textAlign": "center",
+                        "margin": "1%",
+                        "float": "left",
+                    },
+                    # Allow multiple files to be uploaded
+                    multiple=True,
+                ),
+                html.Button(
+                    "Categorize unknown",
+                    id="button_categorize",
+                    style={
+                        "width": "47%",
+                        "height": "60px",
+                        "lineHeight": "60px",
+                        "borderWidth": "2px",
+                        "borderStyle": "solid",
+                        "borderRadius": "5px",
+                        "textAlign": "center",
+                        "margin": "1%",
+                        "float": "left",
+                    },
+                ),
+                dbc.Modal(
+                    [
+                        dbc.ModalHeader("HEADER"),
+                        dbc.ModalBody("BODY OF MODAL"),
+                        dbc.ModalFooter(
+                            dbc.Button(
+                                "CLOSE BUTTON",
+                                id="button_close_modal_categorize",
+                                className="ms-auto",
+                            )
+                        ),
+                    ],
+                    id="modal_categorize",
                 ),
             ],
-        )
-        return children
+            style={"height": "80px"},
+        ),
+        dcc.Graph(
+            id="pie_chart",
+            figure=pie_chart.fig,
+        ),
+    ],
+)
+
+
+@app.callback(Output("pie_chart", "figure"), Input("upload_csv", "contents"))
+def update_csv(contents_list):
+    if contents_list is None:
+        return pie_chart.fig
+
+    # Parse all transactions from csv files.
+    transaction_import_list = []
+    for contents in contents_list:
+        content_type, content_string = contents.split(",")
+        if content_type != "data:text/csv;base64":
+            continue
+        try:
+            decoded_string = io.StringIO(
+                base64.b64decode(content_string).decode("utf-8")
+            )
+            transaction_list = parse_csv(decoded_string)
+        except Exception as e:
+            print(e)
+            pass
+        else:
+            transaction_import_list.extend(transaction_list)
+    
+    # Add transactions to database. This skips transactions that already
+    # exist in the database.
+    with Database(DB_PATH) as db:
+        db.add_transactions(transaction_import_list)
+    
+    # Update figure.
+    pie_chart.update()
+    fig = pie_chart.get_fig()
+
+    return fig
 
 
 @app.callback(
@@ -71,67 +137,4 @@ def toggle_modal(n1, n2, is_open):
 
 
 if __name__ == "__main__":
-    pie_chart = PieChart(DB_PATH)
-    app.layout = html.Div(
-        children=[
-            html.Div(
-                [
-                    dcc.Upload(
-                        id="upload_csv",
-                        children=html.Div(
-                            ["Import CSV files (click or drag and drop)"]
-                        ),
-                        style={
-                            "width": "47%",
-                            "height": "60px",
-                            "lineHeight": "60px",
-                            "borderWidth": "1px",
-                            "borderStyle": "dashed",
-                            "borderRadius": "5px",
-                            "textAlign": "center",
-                            "margin": "1%",
-                            "float": "left",
-                        },
-                        # Allow multiple files to be uploaded
-                        multiple=True,
-                    ),
-                    html.Button(
-                        "Categorize unknown",
-                        id="button_categorize",
-                        style={
-                            "width": "47%",
-                            "height": "60px",
-                            "lineHeight": "60px",
-                            "borderWidth": "2px",
-                            "borderStyle": "solid",
-                            "borderRadius": "5px",
-                            "textAlign": "center",
-                            "margin": "1%",
-                            "float": "left",
-                        },
-                    ),
-                    dbc.Modal(
-                        [
-                            dbc.ModalHeader("HEADER"),
-                            dbc.ModalBody("BODY OF MODAL"),
-                            dbc.ModalFooter(
-                                dbc.Button(
-                                    "CLOSE BUTTON",
-                                    id="button_close_modal_categorize",
-                                    className="ms-auto",
-                                )
-                            ),
-                        ],
-                        id="modal_categorize",
-                    ),
-                ],
-                style={"height": "80px"},
-            ),
-            html.Div(id="csv_output"),
-            dcc.Graph(
-                id="pie_chart",
-                figure=pie_chart.get_fig(),
-            ),
-        ],
-    )
     app.run_server(debug=True)
--- ui/state.py
+++ ui/state.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import pandas as pd
 import plotly.express as px
 from plotly.graph_objects import Figure
@@ -8,38 +10,37 @@ from database import Database
 class PieChart:
     def __init__(self, db_path: str):
         self.db_path = db_path
-        self.category = None
-        self.df = None
-        self.fig = None
-        self.get_df("*")
-        self.get_fig("*")
+        self.category = '*'
+        self.update()
 
-    def get_df(self, category: str = "*") -> pd.DataFrame:
-        if category == self.category and self.df is not None:
-            # Return cached dataframe
-            return self.df
+    def set_category(self, category: str) -> None:
+        if category == self.category:
+            return
+        self.category = category
+        self.update()
+    
+    def get_category(self) -> str:
+        return self.category
 
+    def update(self) -> None:
         with Database(self.db_path) as db:
-            if category == "NULL":
+            if self.category == "NULL":
                 query = f"SELECT * FROM {db.table_name} WHERE category IS NULL"
-            elif category == "*":
+            elif self.category == "*":
                 query = f"SELECT * FROM {db.table_name}"
             else:
                 query = (
-                    f"SELECT * FROM {db.table_name} WHERE category={category}"
+                    f"SELECT * FROM {db.table_name} WHERE category={self.category}"
                 )
-            df = pd.read_sql_query(query, db.connection)
+            self.df = pd.read_sql_query(query, db.connection)
+        self.fig = px.pie(self.df, values="amount", names="name")
 
-        self.category = category
-        self.df = df  # Cache
-        return df
+    def get_df(self, category: Optional[str] = None) -> pd.DataFrame:
+        if category is not None and category != self.category:
+            self.set_category(category)
+        return self.df
 
-    def get_fig(self, category: str = "*") -> Figure:
-        if category == self.category and self.fig is not None:
-            # Returned cached fig
-            return self.fig
-
-        df = self.get_df(category)
-        fig = px.pie(df, values="amount", names="name")
-        self.fig = fig  # Cache
-        return fig 
+    def get_fig(self, category: Optional[str] = None) -> Figure:
+        if category is not None and category != self.category:
+            self.set_category(category)
+        return self.fig
