bf97855a615b712eb9f4b88aea1ed6cf3e0f419b
Eugene Vorontsov <eugene.vorontsov@gmail.com>
2023-03-06 00:24:12 -0500

    Fix parsing.

--- parsing.py
+++ parsing.py
@@ -6,10 +6,21 @@ from database import Transaction
 def parse_csv(csv_file_io):
     transaction_list = []
     reader = csv.reader(csv_file_io)
-    for line in reader:
-        transaction = parse_line(line)
-        if transaction is not None:
-            transaction_list.append(transaction)
+    for line_num, line in enumerate(reader):
+        try:
+            transaction = parse_line(line)
+        except ValueError:
+            if line_num != 0:
+                # Sometimes, the first line is the heading. If casting to float
+                # fails on another line, though, raise the error.
+                print(f"Exception in line {line_num}: {line}")
+                raise
+        except Exception:
+            print(f"Exception in line {line_num}: {line}")
+            raise
+        else:
+            if transaction is not None:
+                transaction_list.append(transaction)
     return transaction_list
 
 
@@ -22,6 +33,22 @@ def parse_line(csv_line):
             name=csv_line[1],
             amount=string_to_float(csv_line[2]) + string_to_float(csv_line[3]),
         )
+    if len(csv_line) == 5:
+        if csv_line[4] == "SPREADSHEET":
+            # Rogers Mastercard from spreadsheet
+            return Transaction(
+                date=csv_line[0],
+                name=csv_line[2],
+                amount=string_to_float(csv_line[4]),
+            )
+        else:
+            # CIBC Visa
+            return Transaction(
+                date=csv_line[0],
+                name=csv_line[1],
+                amount=string_to_float(csv_line[2])
+                + string_to_float(csv_line[3]),
+            )
     if len(csv_line) == 12:
         # Rogers Mastercard
         return Transaction(
@@ -29,17 +56,10 @@ def parse_line(csv_line):
             name=csv_line[6],
             amount=string_to_float(csv_line[11]),
         )
-    if len(csv_line) == 5:
-        # Rogers Mastercard from spreadsheet
-        return Transaction(
-            date=csv_line[0],
-            name=csv_line[2],
-            amount=string_to_float(csv_line[4]),
-        )
     return None
 
 
 def string_to_float(string):
     if string == "":
         return 0.0
-    return float(string.strip("$"))
+    return float(string.replace("$", "").replace(",", ""))
